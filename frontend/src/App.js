c097a6785d69c6fecd7a2f7530b22c2d1373fddc
ujuojadi <jessojadi@gmail.com>
2023-09-20 19:08:20 -0500

    Maison Perfume

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header className="App-header">
+        <a href="/">perfume</a>
+      </header>
+      <main>List products</main>
+    </div>
+  );
+}
+
+export default App;
